308218cefb30405f9f0777725195485b90dcb33f
VaftecPeru <vaftecperusac@gmail.com>
2026-09-16 20:57:46 -0500

    fix: cargar ajustes responsive de portada

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,14 +1,13 @@
 // src/pages/HomePage.tsx
 
 import StoreHome from '../components/StoreHome';
-import '../styles/store-home.css'; 
+import '../styles/store-home.css';
+import '../styles/store-home-mobile.css';
 
 const HomePage = () => {
     return (
         <>
-            
             <StoreHome />
-            
         </>
     );
 };
