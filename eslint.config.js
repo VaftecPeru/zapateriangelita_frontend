5438589d5d8c21c6647bf091caad24dcabef1449
VaftecPeru <vaftecperusac@gmail.com>
2026-09-18 16:26:50 -0500

    chore: habilitar lint real para TypeScript y React

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import tseslint from 'typescript-eslint';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
+  },
+  {
+    files: ['src/**/*.{ts,tsx}', 'tests/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    rules: {
+      'no-debugger': 'error',
+    },
+  },
+];
